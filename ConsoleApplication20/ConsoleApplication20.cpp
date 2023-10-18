2338a3cc1faa411473b83df406e3acf9ffe6b667
msi <msi@DESKTOP-Q39FBF1>
2023-10-18 14:43:35 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication20/ConsoleApplication20.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+
+// 1
+/*
+void replace(string str) {
+    for (char c : str) {
+        if (c == '.') {
+            c = '!';
+        }
+    }
+}
+*/
+
+    }
+}
+
